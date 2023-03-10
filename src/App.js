10977023837ff7db3acc7fc9a879eb436bc95993
Carmen1104 <carmencongko11@gmail.com>
2023-03-10 23:24:29 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import AppContent from './components/AppContent';
 import AppHeader from './components/AppHeader';
-//import Profile from "./components/Profile"; <Profile fullname = "Carmen Cleosa" stuID = "2502009601"/>
+import Profile from "./components/Profile";
 import Title from "./components/Title";
 
 function App() {
   return (
     <div>
+      <Profile fullname = "Carmen Cleosa" stuID = "2502009601"/>
       <Title>TODO LIST</Title>
       <div className='container'>
         <AppHeader />
